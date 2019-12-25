32994a01da08ac342c862d5c0e92b47178a43b75
Vadim Kharin <vadim.kharin@codefresh.io>
2019-12-25 11:02:40 +0200

    add test

---
+++ test/sum.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+
+describe('#test()', function() {
+
+    context('without arguments', function() {
+        it('should return 0', function() {
+            expect(0).to.equal(0)
+        })
+    })
+
+    context('with number arguments', function() {
+        it('should return sum of arguments', function() {
+            expect(15).to.equal(15)
+        })
+
+        it('should return argument when only one argument is passed', function() {
+            expect(5).to.equal(5)
+        })
+    })
+})
